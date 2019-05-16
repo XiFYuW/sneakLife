b8df4933180f35e3bb262bdf4b1dd393ea6c7bb2
XiFYuW <xif_yuw@163.com>
2019-05-16 18:17:49 +0800

    新增全局变量

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import $ from 'jquery'
+import {common} from '@/common/common'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min'
 import 'bootstrap-table/dist/bootstrap-table.min.css'
 import 'bootstrap-table/dist/bootstrap-table.min'
 import 'bootstrap-table/dist/locale/bootstrap-table-zh-CN.min'
+
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$jquery = $
+Vue.prototype.$vue = Vue
+Vue.prototype.$common = common
 
 /* eslint-disable no-new */
 new Vue({
