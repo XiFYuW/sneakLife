213f0b47c935d3f42a019901133d76b4bf2bb7a4
XiFYuW <xif_yuw@163.com>
2019-05-16 18:17:16 +0800

    新增modal.vue

---
+++ src/components/function/modal/modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="modal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="gridSystemModalLabel">Modal title</h4>
+        </div>
+        <div class="modal-body">
+          <line-chart></line-chart>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from '../../chart/lineChart'
+export default {
+  name: 'function-modal',
+  components: {LineChart}
+}
+</script>
+
+<style scoped>
+
+</style>
