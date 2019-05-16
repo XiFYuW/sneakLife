52508e0e04843b8ce05b181929afa2bc1e45558e
XiFYuW <xif_yuw@163.com>
2019-05-16 18:16:59 +0800

    新增system.vue

---
+++ src/components/system.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <system-menu></system-menu>
+    <function-modal></function-modal>
+  </div>
+</template>
+
+<script>
+import SystemMenu from './system/menu/menu'
+import FunctionModal from './function/modal/modal'
+export default {
+  name: 'system',
+  components: {
+    SystemMenu,
+    FunctionModal
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
