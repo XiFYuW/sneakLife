676ece4c3a10c34dbbb7574b188d758efbd3e178
XiFYuW <xif_yuw@163.com>
2019-05-20 18:25:49 +0800

    new

---
+++ src/components/system/body/authorityControl/functionConfig/functionConfig.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container-fluid">
+      <div class="col-sm-3 col-md-2 sidebar">
+        <div id="treeView"></div>
+      </div>
+    </div>
+    <div class="container">
+      <table id="treeGrid"></table>
+    </div>
+  </div>
+</template>
+
+<script>
+import {treeView} from '../../../../../common/treeview'
+import {treeGrid} from '../../../../../common/treeGrid'
+
+export default {
+  name: 'function-config',
+  props: {
+    dataUrl: {
+      type: String,
+      required: true
+    }
+  },
+  mounted () {
+    this.$http.get(this.dataUrl).then(resp => {
+      const options = this.$common.parse(resp)
+      // treeView.init(this.$jquery, options)
+    })
+    treeGrid.init(this.$jquery, this.$http, this.$common)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
