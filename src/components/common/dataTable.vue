d2f92a4059c4df015ae6e8a78c9364a4eda2df7d
XiFYuW <xif_yuw@163.com>
2019-05-24 17:52:33 +0800

    new

---
+++ src/components/common/dataTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading" v-if="head">{{head}}</div>
+      <div class="panel-body">
+        <div class="btn-group" role="group" id="toolbar">
+          <button class="btn btn-default" v-bind:class="item.text" v-bind:key="item.text" v-bind:type="item.type"
+                  v-for="item in opera.funBtn" v-on:click="selectMe(item.code, item.text)">
+            <span class="glyphicon" v-bind:class="item.icon" aria-hidden="true"></span> {{item.text}}
+          </button>
+        </div>
+      </div>
+      <table class="table" id="table"></table>
+    </div>
+    <div>
+      <modal-frame v-bind:funIn="opera.funIn"></modal-frame>
+    </div>
+  </div>
+</template>
+<script>
+import {bootstrapTable} from '../../common/bootstrapTable'
+export default {
+  name: 'data-table',
+  data () {
+    return {
+      opera: {},
+      head: {}
+    }
+  },
+  components: {
+    'modal-frame': () => import('./modalFrame')
+  },
+  props: {
+    dataUrl: {
+      type: String,
+      required: true
+    }
+  },
+  mounted () {
+    this.$http.get(this.dataUrl).then(resp => {
+      const body = this.$common.parse(resp)
+      console.log(body)
+      this.opera = body.opera
+      this.head = body.head
+      bootstrapTable.init('table', this.$jquery, body.dataTables)
+    })
+  },
+  methods: {
+    selectMe: function (code, text) {
+      this.$common.selectClickMe(code, 'table', this.$jquery, text, this.opera.funIn)
+    }
+  }
+}
+</script>
