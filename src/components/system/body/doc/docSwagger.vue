41296485a99e9be62b11eca8107804c03c1d8e00
XiFYuW <xif_yuw@163.com>
2020-08-01 19:07:27 +0800

    增加swagger文档组件

---
+++ src/components/system/body/doc/docSwagger.vue
@@ -0,0 +1,33 @@
+<template>
+  <iframe-sn :src="docSwaggerPath" :id="id"></iframe-sn>
+</template>
+
+<script>
+export default {
+  name: 'doc-swagger',
+  data () {
+    return {
+      id: 'doc-swagger',
+      docSwaggerPath: ''
+    }
+  },
+  components: {
+    'iframe-sn': () => import('../../../common/iframeSn')
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  created () {
+    this.$central.send(this.$utils.http, {me: this.item.pageUrl, data: {menuId: this.item.id}}).then(resp => {
+      this.docSwaggerPath = resp.respData.docSwaggerPath
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
